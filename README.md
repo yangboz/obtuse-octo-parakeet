c9b53e1e629268b4f5af37a408ed306015890f38
yangboz <YoungWelle@gmail.com>
2015-11-05 12:06:12 +0800

    ADDED:testing code by example

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Spring-boot+IoT+SwaggerUI RESTful.
 
 # References:
 http://www.silverspoon.io/
+
+https://github.com/px3/bulldog
---
+++ src/main/java/info/smartkit/eip/iot/obtuse_octo_parakeet/App.java
@@ -0,0 +1,21 @@
+package info.smartkit.eip.iot.obtuse_octo_parakeet;
+
+import io.silverspoon.bulldog.core.gpio.DigitalOutput;
+import io.silverspoon.bulldog.core.platform.Board;
+import io.silverspoon.bulldog.core.platform.Platform;
+import io.silverspoon.bulldog.core.util.BulldogUtil;
+import io.silverspoon.bulldog.raspberrypi.RaspiNames;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        //Detect the board we are running on.
+        Board board = Platform.createBoard();
+        //Set up a digital output
+        DigitalOutput digitalOutput = board.getPin(RaspiNames.P1_11).as(DigitalOutput.class);
+        //Blink the LED
+        digitalOutput.high();
+        BulldogUtil.sleepMs(1000);
+        digitalOutput.low();
+    }
+}
